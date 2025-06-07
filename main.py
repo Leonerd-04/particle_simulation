684448c59cd01f64fba2a9ec063ed2a50c5cbda3
leonard04 <leonerd04.2718@gmail.com>
2025-06-06 21:49:53 -0700

    Add a simple example

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import particle
-import matplotlib.pyplot as plt
+from particle import Particle
+from simulation import Simulation
+import numpy as np
 
 
+def main():
+    # The parameters of our simulation
+    # Eventually these will be read from a json
+    params = {
+        "L":    10,                 # The bound size of our simulation; will be [0, L)
+        "num_particles": 10,
+        "dt": 0.01,
+        "D":     5,                 # Diffusion coefficient
+        "p_init": np.random.random  # Initial position function
+    }
+
+    simulation = Simulation(params)
+
+    simulation.run_steps(100)
+
+    simulation.print()
 
 if __name__ == "__main__":
-    pass
+    main()
