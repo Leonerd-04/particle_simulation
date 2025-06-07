406bd4cb5fb91a1a8e8cf7c55756fd4dc15e7f7b
leonard04 <leonerd04.2718@gmail.com>
2025-06-06 21:39:58 -0700

    Add particle position history

--- particle.py
+++ particle.py
@@ -1,26 +1,23 @@
 import numpy as np
 
 class Particle:
-    # This coefficient is sqrt(2 * D * dt).
-    # Since D and dt generally stay constant, we only compute it once and store the result.
-    COEFFICIENT = 1
-
     # We can initialize a particle with some initial conditions
     def __init__ (self, x_i: float):
         self.x = x_i
+        self.history = []
 
-    # Computes the coefficient
-    # D is the diffusion constant
-    # dt is the size of our time step
-    @staticmethod
-    def set_coefficient(D: float, dt: float) -> None:
-        Particle.COEFFICIENT = np.sqrt(2 * D * dt)
+        # Record the initial position
+        self.history.append(self.x)
+
 
     # Updates the position of the particle for one timestep dt.
-    def update(self, L: float) -> None:
-        dx = self.COEFFICIENT * np.random.normal(0, 1)
+    def update(self, L: float, C: float) -> None:
+        dx = C * np.random.normal(0, 1)
         self.x += dx
 
         # Since we have a periodic boundary condition, we clamp x
         # to between 0 and L using the modulus
         self.x = self.x % L
+
+        # This is how we record the movement of the particle
+        self.history.append(self.x)
