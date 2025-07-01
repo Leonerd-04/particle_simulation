45ee16153a093fb0d1760fd035f351c153dfece9
leonard04 <leonerd04.2718@gmail.com>
2025-07-01 10:34:22 -0700

    Copy Task 3 folder for Task 4

---
+++ 04_stochastic_histograms/src/main.py
@@ -0,0 +1,53 @@
+import numpy as np
+import plots
+from simulation import Simulation
+import json
+import os
+
+config_path = "../config.json"
+out_path = "../../out/sim3"
+
+
+def load_config(path: str) -> dict:
+    with open(path, 'r') as file:
+        json_string = file.read()
+
+    return json.loads(json_string)
+
+
+def save_config(config: dict, path: str):
+    with open(path, 'w') as file:
+        json_string = json.dumps(config)
+        file.write(json_string)
+
+
+def main():
+    # The parameters of our simulation
+    params = load_config(config_path)
+
+    # params['p_init'] = lambda: np.random.normal(0.5, 0.01)
+    # params['p_init'] = lambda: 0.5
+    simulation = Simulation(params)
+
+    simulation.run_steps(200)
+    # simulation.print()
+    # simulation.plot()
+
+    # If the out directory doesn't exist yet, create it
+    try:
+        os.mkdir("../../out")
+        print("output directory successfully created")
+    except FileExistsError:
+        print("output directory already exists")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    simulation.save_to(f"{out_path}.json")
+    simulation.save_to(f"{out_path}.txt", as_json=False)
+
+    simulation.plot_hists_generated()
+
+
+
+if __name__ == "__main__":
+    main()
